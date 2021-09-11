60b616d336508504aaebe636a1c9d63f6779acab
AQI <2161738616@qq.com>
2021-09-11 12:56:13 +0800

    首页接口问题

--- src/components/article-default.vue
+++ src/components/article-default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="article-item px-20 py-10 bg-fff flex flex-col flex-row-between">
-    <p class="fs-12 color-333 line-1">暗示法大是大非暗示法大是大非暗示法大是大非暗示法大是大非</p>
+    <p class="fs-12 color-333 line-1">暗好家伙</p>
     <p class="fs-12 color-999 flex">
       <span>有点666</span>
       <span class="ml-20 ">197评论</span>
