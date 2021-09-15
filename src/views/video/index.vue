ad13121d1ec1f92d56cf3c6a9e499902167f3fba
AQI <2161738616@qq.com>
2021-09-15 20:43:58 +0800

    改为多个视频的样式

--- src/views/video/index.vue
+++ src/views/video/index.vue
@@ -1,10 +1,25 @@
 <template>
-  <div class="video-container bg-fff">
-    <video-player  class="video-player vjs-custom-skin"
-      ref="videoPlayer" 
-      :playsinline="true" 
-      :options="playerOptions"
-    ></video-player>
+   <div>
+    <ul class="mui-table-view">
+      <li class="mui-table-view-cell mui-media" v-for="(item,index) in videolist" :key="item.title">
+        <div class="mui-media-body">
+          <p class="mui-ellipsis">
+            <span>标题：{{ item.title }}</span>
+            <span>上传时间：2020.5.21 13:14:88</span>
+          </p>
+        </div>
+        <h1>
+          <video-player
+            class="video-player vjs-custom-skin"
+            ref="videoPlayer"
+            :playsinline="true"
+            :options="playerOptions[index]"
+            @play="onPlayerPlay($event,index)"
+            @pause="onPlayerPause($event)"
+          ></video-player>
+        </h1>
+      </li>
+    </ul>
     <div class="info-wrapper py-15">
       <p class="title fs-16 color-333 line-2 px-10">
         国防部
@@ -38,45 +53,70 @@
   </div>
 </template>
 
-<script src='../utils/moment.js'></script>
 <script>
 export default {
-  name: 'videoIndex',
-  components: {},
-  props: {},
-  data () {
+  data() {
     return {
-      playerOptions: {
-        playbackRates: [0.7, 1.0, 1.5, 2.0], //播放速度
-        autoplay: false, //如果true,浏览器准备好时开始回放。
-        muted: false, // 默认情况下将会消除任何音频。
-        loop: false, // 导致视频一结束就重新开始。
-        preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
-        language: 'zh-CN',
-        aspectRatio: '4:3', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
-        fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
-        sources: [{
-          type: "video/mp4",//这里的种类支持很多种：基本视频格式、直播、流媒体等，具体可以参看git网址项目
-          src: require('./222.mp4') //url地址
-        }],
-        poster: "", //你的封面地址
-        // width: document.documentElement.clientWidth, //播放器宽度
-        notSupportedMessage: '此视频暂无法播放，请稍后再试', //允许覆盖Video.js无法播放媒体源时显示的默认信息。
-        controlBar: {
-          timeDivider: true,
-          durationDisplay: true,
-          remainingTimeDisplay: false,
-          fullscreenToggle: true  //全屏按钮
-        }
-      }
-    }
+      videolist: [
+        { title: "标题A", movie: "./111.mp4" },
+        { title: "标题B", movie: "./222.mp4" }
+      ],
+      playsinline: true,
+      playerOptions: [],
+      options: ""
+    };
   },
-  computed: {},
-  watch: {},
-  created () {},
-  mounted () {},
-  methods: {}
-}
+  created() {
+    this.getMovieList();
+  },
+
+  methods: {
+    getMovieList() {
+    // 这里正常来说应该是从后台获取的数据，以下操作都是在成功的回调函数里
+      for (var i = 0; i < this.videolist.length; i++) {
+          let arrs = {
+            playbackRates: [1.0, 2.0, 3.0], //播放速度
+            autoplay: false, //如果true,浏览器准备好时开始回放。
+            muted: false, // 默认情况下将会消除任何音频。
+            loop: false, // 导致视频一结束就重新开始。
+            preload: "auto", // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
+            language: "zh-CN",
+            aspectRatio: "16:9", // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+            fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
+            sources: [
+              {
+                type: "video/mp4",
+                type: "video/ogg",
+                src: require("./222.mp4") //url地址
+              }
+            ],
+            poster: "", //封面地址
+            notSupportedMessage: "此视频暂无法播放，请稍后再试", //允许覆盖Video.js无法播放媒体源时显示的默认信息。
+            controlBar: {
+              timeDivider: true,
+              durationDisplay: true,
+              remainingTimeDisplay: false,
+              fullscreenToggle: true //全屏按钮
+            }
+          };
+          this.playerOptions.push(arrs);
+        }
+    },
+    onPlayerPlay(player, index) {
+ 
+      
+       var that = this.$refs.videoPlayer;
+       for (let i = 0; i < that.length; i++) {
+          if(i != index)
+          that[i].player.pause()
+       }
+    },
+    onPlayerPause(player) {
+
+      
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
