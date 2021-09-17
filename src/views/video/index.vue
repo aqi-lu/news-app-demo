fac1cf3e536c3273c8fdede89058b9851dadc71c
AQI <2161738616@qq.com>
2021-09-17 08:42:48 +0800

    一些问题。。。

--- src/views/video/index.vue
+++ src/views/video/index.vue
@@ -9,12 +9,12 @@
         :playsinline="true"
         :options="initOptions(item)"
       ></video-player>
-      <div class="operate flex py-10 color-999 fs-16" @click="toDetail">
+      <div class="operate flex py-10 color-999 fs-16">
         <div class="flex-1 flex flex-row-center flex-col-center">
           <van-icon name="thumb-circle-o"></van-icon>
           <span class="ml-5">{{ item.good }}</span>
         </div>
-        <div class="flex-1 flex flex-row-center flex-col-center">
+        <div class="flex-1 flex flex-row-center flex-col-center" @click="toDetail">
           <van-icon name="comment-o"></van-icon>
           <span class="ml-5">{{ item.comment }}</span>
         </div>
@@ -38,12 +38,12 @@ export default {
     // 假装请求获取视频的数据
     setTimeout(() => {
       const data = [
-        { title: '爱的色放垃圾水电费蓝思科技地方', url: '111.mp4', good: 20, comment: 50, forward: 30 },
-        { title: '支持传入图标名称或图片链接，所有可用的图标名称见右侧示例。', url: '222.mp4', good: 11, comment: 33, forward: 12 },
-        { title: '会在图标右上角展示一个小红点', url: '111.mp4', good: 12, comment: 12, forward: 1 },
-        { title: 'Icon 的 color 属性用来设置图标的颜色。', url: '222.mp4', good: 34, comment: 14, forward: 34 },
-        { title: '属性用来设置图标的尺寸大小，默认单位为 px', url: '111.mp4', good: 33, comment: 55, forward: 12 },
-        { title: 'Icon 组件默认引用有赞 CDN 提供的字体文件，', url: '222.mp4', good: 112, comment: 66, forward: 111 }
+        { title: '国防部最新回应', url: '111.mp4', good: 20, comment: 50, forward: 30 },
+        { title: '国防部最新会议', url: '222.mp4', good: 11, comment: 33, forward: 12 },
+        { title: '国防部最新会议', url: '111.mp4', good: 12, comment: 12, forward: 1 },
+        { title: '国防部最新会议', url: '222.mp4', good: 34, comment: 14, forward: 34 },
+        { title: '国防部最新会议', url: '111.mp4', good: 33, comment: 55, forward: 12 },
+        { title: '国防部最新会议', url: '222.mp4', good: 112, comment: 66, forward: 111 }
       ]
       this.list = data
     })
@@ -51,6 +51,10 @@ export default {
   methods: {
     initOptions(item) {
       return{
+        playbackRates: [1.0, 2.0, 3.0], //播放速度
+        autoplay: false, //如果true,浏览器准备好时开始回放。
+        muted: false, // 默认情况下将会消除任何音频。
+        loop: false, // 导致视频一结束就重新开始。
         preload: "auto", // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
         language: "zh-CN",
         aspectRatio: "16:9", // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
@@ -61,7 +65,15 @@ export default {
             type: "video/ogg",
             src: require('./' + item.url) //url地址
           }
-        ]
+        ],
+        poster: "", //封面地址
+        notSupportedMessage: "此视频暂无法播放，请稍后再试", //允许覆盖Video.js无法播放媒体源时显示的默认信息。
+        controlBar: {
+          timeDivider: true,
+          durationDisplay: true,
+          remainingTimeDisplay: false,
+          fullscreenToggle: true //全屏按钮
+        }
       }
     },
     toDetail() {
